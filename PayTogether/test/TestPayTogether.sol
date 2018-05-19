5ac7730f968dc73d273388208f1ec872bbe81e15
LostHawkGSW <soltanieharya@gmail.com>
2018-05-18 17:35:06 -0700

    Add bare bones tests

---
+++ PayTogether/test/TestPayTogether.sol
@@ -0,0 +1,13 @@
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/PayTogether.sol";
+
+contract TestPayTogetherMultiBeneficiary {
+    function beforeEach() {
+
+    }
+
+    function testWithdraw() {
+
+    }
+}
---
+++ PayTogether/test/TestPayTogetherMultiBeneficiary.sol
@@ -0,0 +1,30 @@
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/PayTogether.sol";
+import "../contracts/BasicManyToManyContract.sol";
+
+contract TestPayTogetherMultiBeneficiary {
+    function beforeEach() {
+
+    }
+
+    function testConstructor() {
+
+    }
+
+    function testJoin() {
+
+    }
+
+    function testEnd() {
+
+    }
+
+    function testCurrentCostPerPerson() {
+
+    }
+
+    function testNextCostPerPerson() {
+
+    }
+}
