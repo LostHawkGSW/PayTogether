69836bb302b354cea14e1708c3dad7aae55f399b
LostHawkGSW <soltanieharya@gmail.com>
2018-05-23 10:18:43 -0700

    Add deployment scripts

---
+++ PayTogether/migrations/2_deploy_contracts.js
@@ -0,0 +1,13 @@
+const PayTogether = artifacts.require("./PayTogether.sol")
+const BasicManyToOneContract = artifacts.require("./BasicManyToOneContract.sol")
+
+module.exports = function(deployer) {
+    const totalCost = web3.toWei(1, 'ether');
+    const minUsers = 2;
+    const maxUsers = 4;
+    const autoEndInDays = 7;
+    const manyToMany = false;
+    deployer.deploy(BasicManyToOneContract, totalCost).then(function() {
+        deployer.deploy(PayTogether, BasicManyToOneContract.address, manyToMany, totalCost, minUsers, maxUsers, autoEndInDays);
+    });
+};
